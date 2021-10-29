6a069f45ee6b3b83f6cf3873c22b07f0ad4ddc7d
Logan Lamers <llamers@mcw.edu>
2021-10-29 11:52:54 -0500

    more files are being generated

--- src/main/java/edu/mcw/rgd/europepmc/DataConverter.java
+++ src/main/java/edu/mcw/rgd/europepmc/DataConverter.java
@@ -1,27 +1,38 @@
 package edu.mcw.rgd.europepmc;
 
-import edu.mcw.rgd.dao.impl.GeneDAO;
-import edu.mcw.rgd.dao.impl.ReferenceDAO;
+import edu.mcw.rgd.dao.impl.*;
 import edu.mcw.rgd.datamodel.Gene;
+import edu.mcw.rgd.datamodel.QTL;
 import edu.mcw.rgd.datamodel.Reference;
+import edu.mcw.rgd.datamodel.Strain;
+import edu.mcw.rgd.datamodel.ontologyx.Term;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataConverter {
     private int rgdId;
     private String title;
+    private String pmid = null;
+    private String accId = null;
 
     public DataConverter(){}
 
     public List<DataConverter> getReferences() throws Exception{
         ReferenceDAO rdao = new ReferenceDAO();
+        XdbIdDAO xdbDAO = new XdbIdDAO();
         List<DataConverter> data = new ArrayList<>();
         List<Reference> list  = rdao.getActiveReferences();
 
         for (Reference ref : list){
             DataConverter dc = new DataConverter();
+            try{
+                String pubid = xdbDAO.getXdbIdsByRgdId(2, ref.getRgdId()).get(0).getAccId();
+                dc.setPmid(pubid);
+            }
+            catch (Exception e){
+                dc.setPmid(null);
+            }
             dc.setRgdId(ref.getRgdId());
             dc.setTitle(ref.getTitle());
             data.add(dc);
@@ -33,17 +44,86 @@ public class DataConverter {
     public List<DataConverter> getGenes() throws Exception {
         List<DataConverter> data = new ArrayList<>();
         GeneDAO dao = new GeneDAO();
-        List<Gene> genes = dao.getActiveGenes(3);
+        List<Gene> genes = dao.getAllActiveGenes();
 
-        for (Gene g : genes){
+        for (Gene g : genes) {
             DataConverter dc = new DataConverter();
             dc.setRgdId(g.getRgdId());
             dc.setTitle(g.getSymbol());
             data.add(dc);
         }
-
         return data;
     }
+
+    public List<DataConverter> getDiseaseOntologies() throws Exception{
+        List<DataConverter> data = new ArrayList<>();
+        OntologyXDAO dao = new OntologyXDAO();
+        List<Term> terms = dao.getActiveTerms("RDO");
+
+        for (Term t : terms){
+            DataConverter dc = new DataConverter();
+            dc.setTitle(t.getTerm());
+            dc.setAccId(t.getAccId());
+            data.add(dc);
+        }
+        return data;
+    }
+
+    public List<DataConverter> getGeneOntologies() throws Exception{
+        List<DataConverter> data = new ArrayList<>();
+        OntologyXDAO dao = new OntologyXDAO();
+        List<Term> terms = dao.getActiveTerms("MF");
+
+        for (Term t : terms){
+            DataConverter dc = new DataConverter();
+            dc.setTitle(t.getTerm());
+            dc.setAccId(t.getAccId());
+            data.add(dc);
+        }
+        return data;
+    }
+
+    public List<DataConverter> getMammalianPhenotype() throws Exception{
+        List<DataConverter> data = new ArrayList<>();
+        OntologyXDAO dao = new OntologyXDAO();
+        List<Term> terms = dao.getActiveTerms("MP");
+
+        for (Term t : terms){
+            DataConverter dc = new DataConverter();
+            dc.setTitle(t.getTerm());
+            dc.setAccId(t.getAccId());
+            data.add(dc);
+        }
+        return data;
+    }
+
+    public List<DataConverter> getStrains() throws Exception{
+        List<DataConverter> data = new ArrayList<>();
+        StrainDAO dao = new StrainDAO();
+        List<Strain> strains = dao.getActiveStrains();
+        for (Strain strain : strains){
+            DataConverter dc = new DataConverter();
+            dc.setRgdId(strain.getRgdId());
+            dc.setTitle(strain.getName());
+            data.add(dc);
+        }
+        return data;
+    }
+
+    public List<DataConverter> getQTLs() throws Exception{
+        List<DataConverter> data = new ArrayList<>();
+        QTLDAO dao = new QTLDAO();
+        List<QTL> qtls = dao.getActiveQTLs(3);
+        for (QTL qtl : qtls){
+            DataConverter dc = new DataConverter();
+            dc.setRgdId(qtl.getRgdId());
+            dc.setTitle(qtl.getName());
+            data.add(dc);
+        }
+        return data;
+    }
+
+
 
     public int getRgdId() {
         return rgdId;
@@ -59,5 +139,21 @@ public class DataConverter {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getPmid() {
+        return pmid;
+    }
+
+    public void setPmid(String pmid) {
+        this.pmid = pmid;
+    }
+
+    public String getAccId() {
+        return accId;
+    }
+
+    public void setAccId(String accId) {
+        this.accId = accId;
     }
 }
--- src/main/java/edu/mcw/rgd/europepmc/Manager.java
+++ src/main/java/edu/mcw/rgd/europepmc/Manager.java
@@ -1,20 +1,20 @@
 package edu.mcw.rgd.europepmc;
 
-import edu.mcw.rgd.dao.impl.ReferenceDAO;
-import edu.mcw.rgd.datamodel.Reference;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.FileSystemResource;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.GZIPOutputStream;
 
 public class Manager {
     public String version;
+
+    protected Logger logger = Logger.getLogger("status");
+
     public static void main(String[] args) throws Exception{
         DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
         new XmlBeanDefinitionReader(bf).loadBeanDefinitions(new FileSystemResource("properties/AppConfigure.xml"));
@@ -25,54 +25,117 @@ public class Manager {
                 String file;
                 switch (args[i]){
                     case "--rgdRef":
-                        url = "https://dev.rgd.mcw.edu/rgdweb/report/reference/main.html?id=";
-                        file = "RGDReferences";
+                        url = "https://rgd.mcw.edu/rgdweb/report/reference/main.html?id={temp}";
+                        file = "RGDReferences.xml.gz";
                         manager.run(url,file);
                         break;
                     case "--genes":
-                        url = "https://dev.rgd.mcw.edu/rgdweb/report/gene/main.html?id=";
-                        file = "RGDgenes";
+                        url = "https://rgd.mcw.edu/rgdweb/report/gene/main.html?id={temp}";
+                        file = "RGDgenes.xml.gz";
+                        manager.run(url,file);
+                        break;
+                    case "--strains":
+                        url = "https://rgd.mcw.edu/rgdweb/report/strain/main.html?id={temp}";
+                        file = "RGDstrains.xml.gz";
+                        manager.run(url,file);
+                        break;
+                    case "--qtls":
+                        url = "https://rgd.mcw.edu/rgdweb/report/qtl/main.html?id={temp}";
+                        file = "RGDqtls.xml.gz";
+                        manager.run(url,file);
+                        break;
+                    case "--ontRDO":
+                        url = "https://rgd.mcw.edu/rgdweb/ontology/annot.html?acc_id={temp}&species=All";
+                        file = "RGDdiseaseOntologies.xml.gz";
+                        manager.run(url,file);
+                        break;
+                    case "--ontGO":
+                        url = "https://rgd.mcw.edu/rgdweb/ontology/annot.html?acc_id={temp}";
+                        file = "RGDgeneOntology.xml.gz";
                         manager.run(url,file);
                         break;
                 }
             }
         }
         catch (Exception e){
-            e.printStackTrace();
+           e.printStackTrace();
         }
     }
 
     void run(String url, String file) throws Exception{
+        logger.info(getVersion());
+        logger.info("Creating file(s) start!");
+        create(url,file);
+        logger.info("Creating file(s) end");
+    }
+
+    void create(String url, String file) throws Exception{
         List<DataConverter> list = new ArrayList<>();
         DataConverter dc = new DataConverter();
+        boolean ontology = false;
+        // get data
         switch (file){
-            case "RGDReferences":
+            case "RGDReferences.xml.gz":
                 list = dc.getReferences();
                 break;
-            case "RGDgenes":
+            case "RGDgenes.xml.gz":
                 list = dc.getGenes();
                 break;
+            case "RGDstrains.xml.gz":
+                list = dc.getStrains();
+                break;
+            case "RGDqtls.xml.gz":
+                list = dc.getQTLs();
+                break;
+            case "RGDdiseaseOntologies.xml.gz":
+                list = dc.getDiseaseOntologies();
+                ontology = true;
+                break;
+            case "RGDgeneOntology.xml.gz":
+                list = dc.getGeneOntologies();
+                ontology = true;
+                break;
         }
-        BufferedWriter out = new BufferedWriter(new FileWriter("data\\"+file+".xml"));
+        BufferedWriter out = openOutputFile(file);
         out.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n");
         out.write("<links>\n");
-        // get data
-//        String url = "https://dev.rgd.mcw.edu/rgdweb/report/reference/main.html?id=";
+
         for(int i = 0; i < list.size(); i++){
-            out.write("\t<link providerId=\"1000\">\n"); // 1000 is a placeholder
+            out.write("\t<link providerId=\"2134\">\n"); // 1000 is a placeholder
             out.write("\t\t<resource>\n");
-            out.write("\t\t\t<url>"+ url+list.get(i).getRgdId() +"</url>\n");
+            if (ontology){
+                    url = url.replace("{temp}",list.get(i).getAccId());
+                    out.write("\t\t\t<url>" + url  + "</url>\n");
+            }
+            else {
+                url = url.replace("{temp}", Integer.toString( list.get(i).getRgdId() ) );
+                out.write("\t\t\t<url>" + url + "</url>\n");
+            }
             out.write("\t\t\t<title>"+ list.get(i).getTitle() +"</title>\n");
             out.write("\t\t</resource>\n");
             out.write("\t\t<record>\n");
-            out.write("\t\t\t<source>"+ "RGD" +"</source>\n");
-            out.write("\t\t\t<id>" + list.get(i).getRgdId()+ "</id>\n");
+            if (list.get(i).getPmid() != null){
+                out.write("\t\t\t<source>" + "PubMed" + "</source>\n");
+                out.write("\t\t\t<id>" + list.get(i).getPmid() + "</id>\n");
+            }
+            else {
+                out.write("\t\t\t<source>" + "RGD" + "</source>\n");
+                out.write("\t\t\t<id>" + list.get(i).getRgdId() + "</id>\n");
+            }
             out.write("\t\t</record>\n");
             out.write("\t</link>\n");
         }
         out.write("</links>");
         out.close();
+        logger.info("\tCreated file: "+file);
+    }
 
+    BufferedWriter openOutputFile(String outputFile) throws IOException {
+        if( outputFile.endsWith(".gz") ) {
+            return new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream("data/"+outputFile))));
+        } else {
+            return new BufferedWriter(new FileWriter(outputFile));
+        }
     }
 
     public void setVersion(String version) {
